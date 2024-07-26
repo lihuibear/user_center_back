88efd845baa21f53b3ab91b8d5027617469b21bd
lihuibear4 <lihui030304@163.com>
2024-07-26 19:45:56 +0800

    feat:登录功能

--- src/main/java/com/lihui/user_center_back/service/UserService.java
+++ src/main/java/com/lihui/user_center_back/service/UserService.java
@@ -7,7 +7,20 @@ import com.lihui.user_center_back.model.domain.User;
 * @author lihui
 * @description 针对表【user(用户)】的数据库操作Service
 * @createDate 2024-07-26 19:06:08
+ *
+ * 用户服务
+ * @author lihui
 */
+
 public interface UserService extends IService<User> {
+
+    /**
+     * 用户注册
+     * @param userAccount 用户账户
+     * @param userPassword 用户密码
+     * @param checkPassword 校验密码
+     * @return 新用户id
+     */
+    long userRegister(String userAccount, String userPassword, String checkPassword);
 
 }
--- src/main/java/com/lihui/user_center_back/service/impl/UserServiceImpl.java
+++ src/main/java/com/lihui/user_center_back/service/impl/UserServiceImpl.java
@@ -1,20 +1,80 @@
 package com.lihui.user_center_back.service.impl;
 
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.lihui.user_center_back.model.domain.User;
 import com.lihui.user_center_back.service.UserService;
 
 import com.lihui.user_center_back.mapper.UserMapper;
+import org.apache.commons.lang3.StringUtils;
 import org.springframework.stereotype.Service;
+import org.springframework.util.DigestUtils;
+
+import javax.annotation.Resource;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import static com.sun.javafx.font.FontResource.SALT;
 
 /**
  * @author lihui
+ * @author lihui
  * @description 针对表【user(用户)】的数据库操作Service实现
  * @createDate 2024-07-26 19:06:08
+ * <p>
+ * 用户实现类
  */
 @Service
 public class UserServiceImpl extends ServiceImpl<UserMapper, User>
         implements UserService {
+    @Resource
+    private UserMapper userMapper;
+
+    @Override
+    public long userRegister(String userAccount, String userPassword, String checkPassword) {
+        //1，校验
+        //非空
+        if (StringUtils.isAnyBlank(userAccount, userPassword, checkPassword)) {
+            return -1;
+        }
+        //账户的话不小于4位，密码不小于8位
+        if (userAccount.length() < 4) {
+            return -1;
+        }
+        if (userPassword.length() < 8 || checkPassword.length() < 8) {
+            return -1;
+        }
+        //账户不能包含特殊字符(放在账户不能重复之前，省去一次数据库查询的资源浪费)
+        String validPattern = "[~!@#$%^&*()+=|{}':;',\\[\\].<>/?~！@#￥%……&*（）——+|{}【】‘；：”“’。，、？]";
+        Matcher matcher = Pattern.compile(validPattern).matcher(userAccount);
+        if (!matcher.find()) {
+            return -1;
+        }
+        //账户不能重复
+        QueryWrapper<User> queryWrapper = new QueryWrapper<>();
+        queryWrapper.eq("userAccount", userAccount);
+        long count = userMapper.selectCount(queryWrapper);
+        if (count > 0) {
+            return -1;
+        }
+
+        //密码校验密码相同
+        if (!userPassword.equals(checkPassword)) {
+            return -1;
+        }
+
+        //2.加密
+        String encryptPassword = DigestUtils.md5DigestAsHex((SALT + userPassword).getBytes());
+        //3.插入数据
+        User user = new User();
+        user.setUserAccount(userAccount);
+        user.setUserPassword(encryptPassword);
+        boolean saveResult = this.save(user);
+        if (!saveResult) {
+            return -1;
+        }
+        return user.getId();
+    }
 
 }
 
